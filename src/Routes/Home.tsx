d1b27379b297133be62d5c2921fc875335e30139
kimbeomjun <gimb52043@gmail.com>
2023-05-24 15:36:46 +0900

    query 식별자 설명

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "react-query";
 import { getMovies } from "./api";
 function Home() {
-  
-  // query키는 배열이 될 수도 있다
+
+  // movies라는 데이터 이름의 nowPlaying이라는 식별자를 가진 데이터를 가져오고, 이를 캐싱하여 재사용
+  // 현재 상영 중인(now playing) 영화에 대한 데이터임을 구분하기 위한 식별자
   const { isLoading, data } = useQuery(["movies", "nowPlaying"], getMovies);
   console.log(isLoading, data);
   return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;

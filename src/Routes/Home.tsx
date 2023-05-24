aedd91aa7ec80dbbc9f3816b8c27744c99d24bac
kimbeomjun <gimb52043@gmail.com>
2023-05-24 16:53:08 +0900

    깃 충돌

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { makeImagePath } from "../utilities";
 
 const Wrapper = styled.div`
+height: 200vh;
   background-color: black;
 `;
 const Loader = styled.div`
